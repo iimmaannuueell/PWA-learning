614e759e4dc2dfbf5a6aad67397ac9842f03fd62
Emmanuel Okeke <ebube.okeke@hotmail.com>
2021-01-30 02:03:13 +0100

    progress on service worker

--- sw.js
+++ sw.js
@@ -3,7 +3,11 @@ self.addEventListener('install', evt => {
     console.log('Service work has been installed')
 });
 
-// Activate service worke
+// Activate service worker
 self.addEventListener('activate', evt => {
     console.log('service  worker has bee activate')
+});
+
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt)
 });
